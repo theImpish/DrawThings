f8de17b8f312fa939a320fecdc3e0cba2818bd12
theImpish <jorge.f.martins@outlook.com>
2023-11-28 23:27:49 +0000

    attempt to draw a triangle

--- DrawThings/main.cpp
+++ DrawThings/main.cpp
@@ -21,15 +21,71 @@ int main()
     glewExperimental = GL_TRUE;
     glewInit();
 
-    GLuint vertexBuffer;
-    glGenBuffers(1, &vertexBuffer);
+    float vertices[] = {
+        0.0f,  0.5f, // Vertex 1 (X, Y)
+        0.5f, -0.5f, // Vertex 2 (X, Y)
+       -0.5f, -0.5f  // Vertex 3 (X, Y)
+    };
 
-    printf("%u\n", vertexBuffer);
+    GLuint vbo;
+    glGenBuffers(1, &vbo); 
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    const char* vertexSource = R"glsl(
+        #version 150 core
+
+        in vec2 position;
+
+        void main()
+        {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    )glsl";
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+
+    glCompileShader(vertexShader);
+
+    const char* fragmentSource = R"glsl(
+        #version 150 core
+
+        out vec4 outColor;
+
+    void main()
+    {
+        outColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+    )glsl";
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+
+    glBindFragDataLocation(shaderProgram, 0, "outColor");
+
+    glLinkProgram(shaderProgram);
+
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(posAttrib);
+
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+
+    glBindVertexArray(vao);
 
     while(!glfwWindowShouldClose(window))
     {
         glfwSwapBuffers(window);
         glfwPollEvents();
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(window, GL_TRUE);
