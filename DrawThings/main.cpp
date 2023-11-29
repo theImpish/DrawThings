befc8b203219676356ccb048e38b7072f340365b
theImpish <jorge.f.martins@outlook.com>
2023-11-29 21:29:37 +0000

    draw triangle

--- DrawThings/main.cpp
+++ DrawThings/main.cpp
@@ -1,12 +1,31 @@
+
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 
+// Shaders
+const GLchar* vertexSource = R"glsl(
+	#version 150 core
+	in vec2 position;
+	void main()
+	{
+		gl_Position = vec4(position, 0.0, 1.0);
+	}
+)glsl";
+const GLchar* fragmentSource = R"glsl(
+	#version 150 core
+	out vec4 outColor;
+	void main()
+	{
+		outColor = vec4(1.0, 1.0, 1.0, 1.0);
+	}
+)glsl";
+
 int main()
 {
     glfwInit();
-    
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -18,78 +37,73 @@ int main()
     //GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", glfwGetPrimaryMonitor(), nullptr);
 
     glfwMakeContextCurrent(window);
+
+    // initialize GLEW
     glewExperimental = GL_TRUE;
     glewInit();
 
-    float vertices[] = {
-        0.0f,  0.5f, // Vertex 1 (X, Y)
-        0.5f, -0.5f, // Vertex 2 (X, Y)
-       -0.5f, -0.5f  // Vertex 3 (X, Y)
+    // Create Vertex Array Objec
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Create  a Vertex Buffer Object and copy the vertex data to it
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    GLfloat vertices[] = {
+        0.0f,  0.5f,
+        0.5f, -0.5f,
+       -0.5f, -0.5f
     };
 
-    GLuint vbo;
-    glGenBuffers(1, &vbo); 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    const char* vertexSource = R"glsl(
-        #version 150 core
-
-        in vec2 position;
-
-        void main()
-        {
-            gl_Position = vec4(position, 0.0, 1.0);
-        }
-    )glsl";
-
+    // Create and compile the vertex shader
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexSource, NULL);
-
     glCompileShader(vertexShader);
 
-    const char* fragmentSource = R"glsl(
-        #version 150 core
-
-        out vec4 outColor;
-
-    void main()
-    {
-        outColor = vec4(1.0, 1.0, 1.0, 1.0);
-    }
-    )glsl";
-
+    // Create and compile the fragmetn shader
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
     glCompileShader(fragmentShader);
 
+    // Link the vertex and fragment shader into a shader program
     GLuint shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
-
     glBindFragDataLocation(shaderProgram, 0, "outColor");
-
     glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
 
+    // Specify the Layout of the vertex data
     GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(posAttrib);
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-    GLuint vao;
-    glGenVertexArrays(1, &vao);
-
-    glBindVertexArray(vao);
-
-    while(!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(window))
     {
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(window, GL_TRUE);
+               
+        // Clear the screen to black
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw a triangle from the 3 vertices
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
-     
+
+    glDeleteProgram(shaderProgram);
+    glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
+    glDeleteBuffers(1, &vbo);
+    glDeleteVertexArrays(1, &vao);
+
     glfwTerminate();
 }
